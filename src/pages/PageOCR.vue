9c315a4274c7a687dca51f697d45e407f0649d26
MuhSatriaOnero <satria@team.onero.id>
2019-12-19 16:28:16 +0700

    read string to date

--- src/pages/PageOCR.vue
+++ src/pages/PageOCR.vue
@@ -9,6 +9,12 @@
       <div>
         <p v-text="message">halo</p>
         <p v-text="passport"></p>
+        <p v-text="name"></p>
+        <div>
+          <p>{{ year[0] + ' Tanggal Lahir' }}</p>
+          <p>{{ year[1] + ' Pembuatan' }}</p>
+          <p>{{ year[2] + ' Expired' }}</p>
+        </div>
       </div>
       <!-- <div @click="checkValue">check aku</div> -->
       <!-- <q-img id="text-img" alt="Vue logo" src="https://2.bp.blogspot.com/-UtaWQXWKSro/WGE_CwX-wwI/AAAAAAAAdFY/CbAcpOphEjY7T1pTTl6wtshYviNwSmk5wCLcB/s1600/passport.jpg" /> -->
@@ -30,7 +36,10 @@ export default {
       message: [],
       passport: '',
       text: '',
-      gender: ''
+      gender: '',
+      name: '',
+      year : [],
+      pureYear: []
     }
   },
   methods: {
@@ -73,20 +82,34 @@ export default {
       // debugger
       console.log(text)
       // var female = text.search('P')
+      var date = new Date();
+      for( var i = 1920; i<= date.getFullYear() + 5 ; i ++) {
+        var searchYear = text.search(i)
+        if(searchYear != -1 && text.charAt(searchYear - 1 ) == " ") {
+          var pureYear = text.substring(searchYear-7,searchYear + 4);
+          this.year.push(pureYear)
+        }
+      }
       for (var i = 0;  i < text.length; i++) {
         if (text[i] === 'P') {
           var nexIndex = i + 2
           if (text.charAt(nexIndex) === 'F') {
             this.gender = 'female'
+            debugger
+            this.name = text.slice(55, nexIndex)
           }
         } else if (text[i] === 'L') {
           var nexIndex = i + 2
           if (text.charAt(nexIndex) === 'M') {
             this.gender = 'male'
+            this.name = text.slice(55, nexIndex)
           }
         }
-      } 
+
+      }
+      console.log(this.year);
       console.log(this.gender)
+      console.log(this.name)
       // debugger
       // console.log(text.search('p'))
       self.message = changedText
